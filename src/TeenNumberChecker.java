a4b199ae49b4e819b37fc460cf4f7c3fe62fc781
Samuel Titiloye <samuelotitiloye@gmail.com>
2020-06-20 11:35:19 -0500

    check if value range is between thirteen and nineteen

---
+++ src/TeenNumberChecker.java
@@ -0,0 +1,45 @@
+public class TeenNumberChecker {
+
+    public static boolean hasTeen(int firstValue, int secondValue, int thirdValue) {
+        if (isTeen(firstValue)) {
+            return true;
+        }
+        if (isTeen(secondValue)) {
+            return true;
+        }
+        if (isTeen(thirdValue)) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isTeen(int teenChecker) {
+        if (teenChecker >= 13 && teenChecker <= 19) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hasTeen(22, 23, 34));
+    }
+}
+
+
+// We'll say that a number is "teen" if it is in the range 13 -19 (inclusive).
+// Write a method named hasTeen with 3 parameters of type int.
+// The method should return boolean and it needs to return true if one of
+// the parameters is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
+
+//        EXAMPLES OF INPUT/OUTPUT:
+// * hasTeen(9, 99, 19);  should return true since 19 is in range 13 - 19
+// * hasTeen(23, 15, 42);  should return true since 15 is in range 13 - 19
+// * hasTeen(22, 23, 34);  should return false since numbers 22, 23, 34 are not in range 13-19
+
+// Write another method named isTeen with 1 parameter of type int.
+// The method should return boolean and it needs to return true if the parameter
+// is in range 13(inclusive) - 19 (inclusive). Otherwise return false.
+
+//        EXAMPLES OF INPUT/OUTPUT:
+// * isTeen(9);  should return false since 9 is in not range 13 - 19
+// * isTeen(13);  should return true since 13 is in range 13 - 19
